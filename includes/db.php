f693e5f859a4c64a23f00fa38f878c5bc88aaca7
Tong Lin <wowawawoa@gmail.com>
2023-10-25 16:55:49 +1030

    feat: connect db and restructure index page

---
+++ includes/db.php
@@ -0,0 +1,23 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "cms_blogsystem");
+define("DB_PORT", 3307);
+
+// $db['DB_HOST'] = "localhost";
+// $db['DB_USER'] = "root";
+// $db['DB_PASS'] = "";
+// $db['DB_NAME'] = "cms_blogsystem";
+// $db['DB_PORT'] = 3307;
+
+// foreach ($db as $key => $value) {
+//   define($key, $value);
+// }
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
+if ($connection) {
+  echo "We are connected";
+}
