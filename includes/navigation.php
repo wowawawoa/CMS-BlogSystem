269ad3b4d254de1d5bd7db26bf82ef661e97711a
Tong Lin <wowawawoa@gmail.com>
2023-10-26 13:41:10 +1030

    refactor: refactor admin index

--- includes/navigation.php
+++ includes/navigation.php
@@ -13,7 +13,9 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
+
         <?php
+
         $query = "SELECT * FROM categories";
         $select_all_categories_query = mysqli_query($connection, $query);
 
@@ -21,16 +23,13 @@
           $cat_title = $row['cat_title'];
           echo "<li><a href='#'>{$cat_title}</a></li>";
         }
+
         ?>
-        <!-- <li>
-          <a href="#">About</a>
-        </li>
+
         <li>
-          <a href="#">Services</a>
+          <a href='admin'>Admin</a>
         </li>
-        <li>
-          <a href="#">Contact</a>
-        </li> -->
+
       </ul>
     </div>
     <!-- /.navbar-collapse -->
