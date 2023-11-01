20c4ec0606b1ed6cdba28cf8476f15bbe99e97a0
Tong Lin <wowawawoa@gmail.com>
2023-11-01 20:16:30 +1030

    feat: adding loading effect in admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -34,6 +34,7 @@ if (!isset($_SESSION['user_role'])) {
 
   <!-- Custom CSS -->
   <link href="css/sb-admin.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
 
   <!-- Custom Fonts -->
   <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
